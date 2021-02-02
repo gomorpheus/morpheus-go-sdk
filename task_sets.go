8ba71e836bceeb75666fade4b51ce046d387f719
martezr <martez.reed@greenreedtech.com>
2021-02-01 22:37:31 -0600

    Add additional resources

--- task_sets.go
+++ task_sets.go
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // Morpheus API types and Client methods for Task Sets
-=======
-// Morpheus API types and Client methods for Option Types
->>>>>>> 2a75585 (Add additional resources)
-=======
-// Morpheus API types and Client methods for Task Sets
->>>>>>> 57ff49e (Update task sets)
+
 package morpheus
 
 import (
@@ -26,11 +19,7 @@ type TaskSet struct {
 	Name        string        `json:"name"`
 	Description string        `json:"description"`
 	Type        string        `json:"type"`
-<<<<<<< HEAD
 	OptionTypes []interface{} `json:"optionTypes"`
-=======
-	OptionTypes []int64       `json:"optionTypes"`
->>>>>>> 2a75585 (Add additional resources)
 	Tasks       []interface{} `json:"types"`
 }
 
@@ -135,11 +124,7 @@ func (client *Client) FindTaskSetByName(name string) (*Response, error) {
 	listResult := resp.Result.(*ListTaskSetsResult)
 	taskSetCount := len(*listResult.TaskSets)
 	if taskSetCount != 1 {
-<<<<<<< HEAD
 		return resp, fmt.Errorf("found %d TaskSets for %v", taskSetCount, name)
-=======
-		return resp, fmt.Errorf("Found %d TaskSets for %v", taskSetCount, name)
->>>>>>> 2a75585 (Add additional resources)
 	}
 	firstRecord := (*listResult.TaskSets)[0]
 	taskSetID := firstRecord.ID
--- task_sets_test.go
+++ task_sets_test.go
@@ -6,13 +6,6 @@ import (
 	"github.com/gomorpheus/morpheus-go-sdk"
 )
 
-<<<<<<< HEAD
-=======
-var (
-	testTaskSetName = "golangtestworkflow"
-)
-
->>>>>>> 2a75585 (Add additional resources)
 func TestListTaskSets(t *testing.T) {
 	client := getTestClient(t)
 	req := &morpheus.Request{}
@@ -39,62 +32,5 @@ func TestGetTaskSet(t *testing.T) {
 
 		// List by name
 
-<<<<<<< HEAD
 	}
-=======
-	} else {
-
-	}
-}
-
-func TestTaskSetsCRUD(t *testing.T) {
-	client := getTestClient(t)
-	//create
-	// this has no uniqueness check on name, it probably should..
-	req := &morpheus.Request{
-		Body: map[string]interface{}{
-			"taskSet": map[string]interface{}{
-				"name":             testNetworkDomainName,
-				"description":      "a test domain",
-				"publicZone":       false,
-				"domainController": false,
-				"visibility":       "private",
-			},
-		},
-	}
-	resp, err := client.CreateTaskSet(req)
-	result := resp.Result.(*morpheus.CreateTaskSetResult)
-	assertResponse(t, resp, err)
-	assertNotNil(t, result)
-	assertEqual(t, result.Success, true)
-	assertNotNil(t, result.TaskSet)
-	assertNotEqual(t, result.TaskSet.ID, 0)
-	assertEqual(t, result.TaskSet.Name, testNetworkDomainName)
-
-	// update
-	updateReq := &morpheus.Request{
-		Body: map[string]interface{}{
-			"taskSet": map[string]interface{}{
-				"description": "my new description",
-			},
-		},
-	}
-	updateResp, updateErr := client.UpdateTaskSet(result.TaskSet.ID, updateReq)
-	updateResult := updateResp.Result.(*morpheus.UpdateTaskSetResult)
-	assertResponse(t, updateResp, updateErr)
-	assertNotNil(t, updateResult)
-	assertEqual(t, updateResult.Success, true)
-	assertNotNil(t, updateResult.TaskSet)
-	assertNotEqual(t, updateResult.TaskSet.ID, 0)
-	assertEqual(t, updateResult.TaskSet.Description, "my new description")
-
-	// delete
-	deleteReq := &morpheus.Request{}
-	deleteResp, deleteErr := client.DeleteTaskSet(result.TaskSet.ID, deleteReq)
-	deleteResult := deleteResp.Result.(*morpheus.DeleteTaskSetResult)
-	assertResponse(t, deleteResp, deleteErr)
-	assertNotNil(t, deleteResult)
-	assertEqual(t, deleteResult.Success, true)
-
->>>>>>> 2a75585 (Add additional resources)
 }
