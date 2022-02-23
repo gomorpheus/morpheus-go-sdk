14b45acd63d5b4b6850432552efb3f40a9f67d3b
Martez Reed <martez.reed@greenreedtech.com>
2022-02-23 16:11:46 -0600

    Add support for roles

---
+++ roles_test.go
@@ -0,0 +1,33 @@
+package morpheus_test
+
+import (
+	"testing"
+
+	"github.com/gomorpheus/morpheus-go-sdk"
+)
+
+func TestListRoles(t *testing.T) {
+	client := getTestClient(t)
+	req := &morpheus.Request{}
+	resp, err := client.ListRoles(req)
+	assertResponse(t, resp, err)
+}
+
+func TestGetRole(t *testing.T) {
+	client := getTestClient(t)
+	req := &morpheus.Request{}
+	resp, err := client.ListRoles(req)
+	assertResponse(t, resp, err)
+
+	// parse JSON and fetch the first one by ID
+
+	result := resp.Result.(*morpheus.ListRolesResult)
+	recordCount := result.Meta.Total
+	t.Logf("Found %d Roles.", recordCount)
+	if recordCount != 0 {
+		// Get by ID
+		record := (*result.Roles)[0]
+		resp, err = client.GetRole(record.ID, &morpheus.Request{})
+		assertResponse(t, resp, err)
+	}
+}
