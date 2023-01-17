c6636b91cfffb53da55c21b467906475743cfaae
Mike Garuccio <mike.garuccio@expedient.com>
2023-01-17 14:39:14 -0500

    initial add of execution request support

---
+++ execution_request.go
@@ -0,0 +1,78 @@
+package morpheus
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var (
+	// ExecutionRequestPath is the API endpoint for execution requests
+	ExecutionRequestPath = "/api/execution-request"
+)
+
+// Execution Request strcutures for use in request and response payloads
+type ExecutionRequest struct {
+	Script string `json:"script"`
+}
+
+// ExeuctionRequestResult structure parses the response from the server when running or retrieving an exeuction request
+type ExecutionRequestResult struct {
+	ID            int64    `json:"id"`
+	UniqueID      string   `json:"uniqueId"`
+	ContainerID   int64    `json:"containerId"`
+	ServerID      int64    `json:"serverId"`
+	InstanceID    int64    `json:"instanceId"`
+	ResourceID    int64    `json:"resourceId"`
+	AppID         int64    `json:"appId"`
+	StdOut        string   `json:"stdOut"`
+	StdErr        string   `json:"stdErr"`
+	ExitCode      int64    `json:"exitCode"`
+	Status        string   `json:"status"`
+	ExpiresAt     string   `json:"expiresAt"`
+	CreatedById   int64    `json:"createdById"`
+	StatusMessage string   `json:"statusMessage"`
+	ErrorMessage  string   `json:"errorMessage"`
+	Config        struct{} `json:"config"`
+	RawData       string   `json:"rawData"`
+}
+
+// Execution Request Methods
+
+func (client *Client) GetExecutionRequest(uniqueID string, req *Request) (*Response, error) {
+	return client.Execute(&Request{
+		Method:      "GET",
+		Path:        fmt.Sprintf("%s/%s", ExecutionRequestPath, uniqueID),
+		QueryParams: req.QueryParams,
+		Result:      &ExecutionRequestResult{},
+	})
+}
+
+func (client *Client) CreateExecutionRequest(req *Request) (*Response, error) {
+	return client.Execute(&Request{
+		Method:      "POST",
+		Path:        fmt.Sprintf("%s", ExecutionRequestPath),
+		QueryParams: req.QueryParams,
+		Body:        req.Body,
+		Result:      &ExecutionRequestResult{},
+	})
+}
+
+// helper functions
+
+func (client *Client) ExecuteScriptOnInstance(instance Instance, script string) (*Response, error) {
+
+	resp, err := client.CreateExecutionRequest(&Request{
+		QueryParams: map[string]string{
+			"instanceId": strconv.Itoa(int(instance.ID)),
+		},
+		Body: map[string]interface{}{
+			"script": script,
+		},
+	})
+
+	if err != nil {
+		fmt.Println("unable to run script on instance ", instance.ID, " due to ", err)
+	}
+
+	return resp, err
+}
---
+++ execution_request_test.go
@@ -0,0 +1,29 @@
+package morpheus_test
+
+import (
+	"testing"
+
+	"github.com/gomorpheus/morpheus-go-sdk"
+)
+
+// not clear how to test the "get" function since it requires a uniqueID and there is no list method
+// func TestGetExecutionRequest(t *testing.T) {
+
+// }
+
+func TestExecuteScriptOnInstance(t *testing.T) {
+	client := getTestClient(t)
+	resp, err := client.ListInstances(&morpheus.Request{})
+	assertResponse(t, resp, err)
+
+	// parse JSON and fetch the first one by ID
+	listInstancesResult := resp.Result.(*morpheus.ListInstancesResult)
+	instancesCount := listInstancesResult.Meta.Total
+	t.Logf("Found %d Instances.", instancesCount)
+
+	if instancesCount != 0 {
+		firstInstance := (*listInstancesResult.Instances)[0]
+		resp, err = client.ExecuteScriptOnInstance(firstInstance, "pwd")
+		assertResponse(t, resp, err)
+	}
+}
