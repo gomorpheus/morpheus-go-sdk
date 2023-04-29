087dbd7fc014e7fd7dc86443e5d3b9fef9072fd1
StefanBerner <stefan@berner.ws>
2023-04-29 12:46:50 +0000

    Added CreateKeyPair and DeleteKeyPair function

--- key_pairs.go
+++ key_pairs.go
@@ -57,7 +57,7 @@ type UpdateKeyPairBody struct {
 	CreateKeyPairPayload
 }
 
-func (client *Client) ListKeyPairs(req *Request) (*Response, error) {
+func (client *Client) ListKeyPairs() (*Response, error) {
 	return client.Execute(&Request{
 		Method: "GET",
 		Path:   KeyPairsPath,
@@ -65,10 +65,30 @@ func (client *Client) ListKeyPairs(req *Request) (*Response, error) {
 	})
 }
 
-func (client *Client) GetKeyPair(id int64, req *Request) (*Response, error) {
+func (client *Client) GetKeyPair(id int64) (*Response, error) {
 	return client.Execute(&Request{
 		Method: "GET",
 		Path:   fmt.Sprintf("%s/%d", KeyPairsPath, id),
 		Result: &GetKeyPairResult{},
+	})
+}
+func (client *Client) CreateKeyPair(name string, publicKey string) (*Response, error) {
+	req := &Request{
+		Method: "POST",
+		Path:   "/api/key-pairs",
+		Body: map[string]interface{}{
+			"keyPair": map[string]string{
+				"name":      "test123",
+				"publicKey": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJzKbAk5Yu1YDNTcr4IxKOcvO3nZtCbRECons3Hl7SWCgX1+aj1BANJY0DTmpjbQ+rv+TkbWrQePwRy+HR4s6So4GJImlyOLmFMH5qJOJRMCoi3vUgyFa0cluT4G9MBK66ym1kYB3ZXh3RAMwmOY3jKmjRAA5282K3F+UT+bedihQZuuW0IIOrnLD1kFg39xB8XJsu0ysym1L1tEfh19i15bciQdESsRf0ClAn8ELgn6LZ/VYS5cOr03wa0VZojwF+GrxHEvYNP0KKvUb9Sgt7grlfieWf0f8LHdS6vbNNhumvzGJWHU9Ak6jNkvXO3aHaGhOUtoZYtXhYS96C8FlatZGHKnhMfkkRWcujW7lB4xujPs7R0yzOCwHZ65BRCWk1qn2yZti8OYhl4uZ4arUOMhXvOndLKOGBf1tShLrNxATs4Fe5L0G0B6iL0xJ8Xc4m1ppfot91hRwyFzn4QuVDnlr4zKyQI7JhOhwprwJDPreKi+iUPtRVxweXg98hOK0= stefan@console",
+			},
+		},
+	}
+	return client.Execute(req)
+}
+func (client *Client) DeleteKeyPair(id int64) (*Response, error) {
+	return client.Execute(&Request{
+		Path:   fmt.Sprintf("%s/%d", KeyPairsPath, id),
+		Method: "DELETE",
+		Result: &DeleteKeyPairResult{},
 	})
 }
