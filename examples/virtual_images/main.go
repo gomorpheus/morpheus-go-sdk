36443c737b4c7b3a1d0dc081bb456700290615ae
Martez Reed <martez.reed@greenreedtech.com>
2024-09-08 21:09:18 -0500

    Update network pool server payload

--- examples/virtual_images/main.go
+++ examples/virtual_images/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	b64 "encoding/base64"
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/gomorpheus/morpheus-go-sdk"
 )
@@ -15,64 +16,64 @@ func main() {
 		fmt.Println("LOGIN ERROR: ", err)
 	}
 	fmt.Println("LOGIN RESPONSE:", resp)
-
-	// List virtual images
-	req := &morpheus.Request{
-		QueryParams: map[string]string{"filterType": "All"},
-	}
-	response, err := client.ListVirtualImages(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	result := response.Result.(*morpheus.ListVirtualImagesResult)
-	log.Println(result.VirtualImages)
 	/*
-	   	createReq := &morpheus.Request{
-	   		Body: map[string]interface{}{
-	   			"virtualImage": map[string]interface{}{
-	   				"name":            "demoimage",
-	   				"imageType":       "iso",
-	   				"isCloudInit":     false,
-	   				"installAgent":    false,
-	   				"virtioSupported": false,
-	   			},
-	   		},
-	   	}
-
-	   createResp, err := client.CreateVirtualImage(createReq)
-
-	   	if err != nil {
-	   		fmt.Println(err)
-	   	}
-
-	   createImageResult := createResp.Result.(*morpheus.CreateVirtualImageResult)
-	   fmt.Println(createImageResult)
-
-	   // Upload Virtual Image
-	   virtualImageName := "netboot.xyz.iso"
-	   virtualImagePath := fmt.Sprintf("/Downloads/%s", virtualImageName)
-	   data, err := os.ReadFile(virtualImagePath)
-
-	   	if err != nil {
-	   		fmt.Println(err)
-	   	}
-
-	   sEnc := b64.StdEncoding.EncodeToString([]byte(data))
-	   fmt.Println("Base64Encoding")
-
-	   	uploadResp, err := client.UploadVirtualImage(createImageResult.VirtualImage.ID, &morpheus.Request{
-	   		QueryParams: map[string]string{
-	   			"filename": virtualImageName,
-	   		},
-	   		IsStream:   true,
-	   		StreamBody: fmt.Sprintf("data:application/octet-stream;name=%s;base64,%s", virtualImageName, sEnc),
-	   	})
-
-	   	if err != nil {
-	   		fmt.Println(err)
-	   	}
-
-	   uploadImageResult := uploadResp.Result.(*morpheus.UploadVirtualImageResult)
-	   fmt.Println(uploadImageResult)
+		// List virtual images
+		req := &morpheus.Request{
+			QueryParams: map[string]string{"filterType": "All"},
+		}
+		response, err := client.ListVirtualImages(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		result := response.Result.(*morpheus.ListVirtualImagesResult)
+		log.Println(result.VirtualImages)
 	*/
+
+	createReq := &morpheus.Request{
+		Body: map[string]interface{}{
+			"virtualImage": map[string]interface{}{
+				"name":            "demoimage",
+				"imageType":       "iso",
+				"isCloudInit":     false,
+				"installAgent":    false,
+				"virtioSupported": false,
+			},
+		},
+	}
+
+	createResp, err := client.CreateVirtualImage(createReq)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	createImageResult := createResp.Result.(*morpheus.CreateVirtualImageResult)
+	fmt.Println(createImageResult)
+
+	// Upload Virtual Image
+	virtualImageName := "netboot.xyz.iso"
+	virtualImagePath := fmt.Sprintf("/Downloads/%s", virtualImageName)
+	data, err := os.ReadFile(virtualImagePath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+
+	uploadResp, err := client.UploadVirtualImage(createImageResult.VirtualImage.ID, &morpheus.Request{
+		QueryParams: map[string]string{
+			"filename": virtualImageName,
+		},
+		IsStream:   true,
+		StreamBody: fmt.Sprintf("data:application/octet-stream;name=%s;base64,%s", virtualImageName, sEnc),
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	uploadImageResult := uploadResp.Result.(*morpheus.UploadVirtualImageResult)
+	fmt.Println(uploadImageResult)
+
 }
